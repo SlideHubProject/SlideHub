d307f136c4b4d266b68d7089c452eaad695b7ab7
JersonMolina <molinajerson885@gmail.com>
2026-04-06 13:06:33 -0500

    se terminan de subir los servicos

---
+++ slidehub-monolith/src/main/java/com/brixo/slidehub/state/service/SlideStateService.java
@@ -0,0 +1,20 @@
+package com.brixo.slidehub.state.service;
+
+import com.brixo.slidehub.state.model.SlideStateResponse;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+
